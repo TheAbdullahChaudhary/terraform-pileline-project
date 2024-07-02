61ac4084b5b7566ddc51610a0f11c58a0c63bc7c
Muhammad Abdullah <97453802+TheAbdullahChaudhary@users.noreply.github.com>
2024-07-03 01:56:27 +0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,20 +2,19 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-
 resource "aws_instance" "my_instance" {
   ami                         = "ami-0f58b397bc5c1f2e8"
   associate_public_ip_address = true
   availability_zone           = "ap-south-1b"
   instance_type               = "t2.micro"
   key_name                    = "abdullah_key_new"
-  subnet_id                   = "subnet-021075ff329a6aa2f"
+  subnet_id                   = aws_subnet.public_subnet.id
 
   tags = {
     "Name" = "terraform-ec2"
   }
 
-  vpc_security_group_ids = ["sg-092a8702ace82fd27"]
+  vpc_security_group_ids = [aws_security_group.my_security_group.id]
 }
 
 resource "aws_internet_gateway" "my_igw" {
@@ -23,7 +22,7 @@ resource "aws_internet_gateway" "my_igw" {
     "Name" = "terraform-gateway"
   }
 
-  vpc_id = "vpc-0935a5ad02d577264"
+  vpc_id = aws_vpc.my_vpc.id
 }
 
 resource "aws_route_table" "public_route_table" {
@@ -31,7 +30,12 @@ resource "aws_route_table" "public_route_table" {
     "Name" = "terraform-route-table"
   }
 
-  vpc_id = "vpc-0935a5ad02d577264"
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my_igw.id
+  }
 }
 
 resource "aws_subnet" "public_subnet" {
@@ -41,7 +45,7 @@ resource "aws_subnet" "public_subnet" {
     "Name" = "terraform-subnet"
   }
 
-  vpc_id = "vpc-0935a5ad02d577264"
+  vpc_id = aws_vpc.my_vpc.id
 }
 
 resource "aws_vpc" "my_vpc" {
@@ -50,5 +54,34 @@ resource "aws_vpc" "my_vpc" {
 
   tags = {
     "Name" = "terraform-vpc"
+  }
+}
+
+resource "aws_security_group" "my_security_group" {
+  name        = "terraform-sg"
+  description = "Allow SSH and HTTP inbound traffic"
+  vpc_id      = aws_vpc.my_vpc.id
+
+  ingress {
+    description = "SSH from anywhere"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP from anywhere"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
