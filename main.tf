6c88cf5719b46046fa919864470acccbc64936fa
Muhammad Abdullah <97453802+TheAbdullahChaudhary@users.noreply.github.com>
2024-07-03 01:53:39 +0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,29 +2,6 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_iam_role" "my_role" {
-  assume_role_policy = jsonencode({
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      },
-    ]
-    Version = "2012-10-17"
-  })
-
-  description          = "Allows EC2 instances to call AWS services on your behalf."
-  managed_policy_arns  = ["arn:aws:iam::aws:policy/AdministratorAccess"]
-  max_session_duration = 3600
-  name                 = "terraform-role"
-}
-
-resource "aws_iam_user" "my_user" {
-  name = "abdullah-terraform"
-}
 
 resource "aws_instance" "my_instance" {
   ami                         = "ami-0f58b397bc5c1f2e8"
